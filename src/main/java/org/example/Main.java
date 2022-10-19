8c360f5407cd2b7909300f1a1dc2db8d81b90967
Eugene Kos <mstrsid@live.com>
2022-10-19 12:39:02 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,38 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
+    List<User> users = new ArrayList<>();
+    users.add(new User("Alex", 20));
+    users.add(new User("Mike", 35));
+    users.add(new User("Olha", 19));
+    users.add(new User("Bob", 38));
+    users.add(new User("John", 34));
+    users.add(new User("Ann", 31));
 
-    List<Integer> numbers = new ArrayList<>();
-    for (int i = 0; i < 100; i++) {
-      numbers.add((int) (Math.random() * 1000));
-    }
+    long count = users.stream().filter(user -> user.getAge() > 30).count();
+    System.out.println(count);
 
-    List<String> res = numbers.stream().filter((Integer n) -> n % 2 == 0).map(integer -> "Number: " + integer)
-        .filter(string -> string.endsWith("0")).map(string -> string + "!").collect(Collectors.toList());
+    boolean res1 = users.stream().allMatch(user -> user.getName().contains("i"));
+    System.out.println(res1);
 
-    for(String s: res){
-      System.out.println(s);
-    }
-  }
+    boolean res2 = users.stream().anyMatch(user -> user.getName().contains("i"));
+    System.out.println(res2);
 
-  private static List<String> map(List<Integer> numbers) {
-    List<String> strings = new ArrayList<>();
-    for (int n : numbers) {
-      strings.add("Number: " + n);
-    }
-    return strings;
-  }
+    boolean res3 = users.stream().noneMatch(user -> user.getName().contains("i"));
+    System.out.println(res3);
 
-  private static List<Integer> filter(List<Integer> list, Predicate<Integer> test) {
-    List<Integer> res = new ArrayList<>();
-    for (int n : list) {
-      if (test.test(2)) {
-        res.add(n);
-      }
-    }
-    return res;
+    users.stream()
+        .sorted((user1, user2) -> Integer.compare(user2.getAge(), user1.getAge())).limit(3)
+        .forEach(System.out::println);
+
+    users.stream().sorted(Comparator.comparing(User::getName))
+        .filter(user -> user.getAge() > 30).limit(3).map(User::getName).forEach(
+            System.out::println);
   }
 }
